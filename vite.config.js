a39a35eea9d4969b2f64b42acd7bb45d41582ff1
Sheelam Vihas Reddy <vihasreddy111@gmail.com>
2025-06-07 10:56:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MyPortfolio/', // ✅ use repo name with leading & trailing slash
+  base: '/', // ✅ use repo name with leading & trailing slash
 })
