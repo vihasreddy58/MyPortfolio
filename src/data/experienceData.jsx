06c9e915370eefa379a727ef6e6be27a1002fe28
vihasreddy58 <vihasreddy111@gmail.com>
2025-06-13 14:39:34 +0530

    updated certifications

--- src/data/experienceData.jsx
+++ src/data/experienceData.jsx
@@ -3,25 +3,32 @@ export const experienceData = [
         id: 1,
         company: 'MongoDB University',
         jobtitle: 'Certified Associate Developer',
+        startYear: '2025',
+        endYear: '2025'
+    },
+    {
+        id: 2,
+        company: 'Oracle',
+        jobtitle: 'OCI AI Foundations Associate',
+        startYear: '2025',
+        endYear: '2025'
+    },
+    {
+        id: 3,
+        company: 'Infosys Springboard',
+        jobtitle: 'Java Foundation Certification',
         startYear: '2024',
         endYear: '2024'
     },
     {
-        id: 2,
-        company: 'Infosys Springboard',
-        jobtitle: 'Java Foundation Certification',
-        startYear: '2023',
-        endYear: '2023'
-    },
-    {
-        id: 3,
+        id: 4,
         company: 'NPTEL',
         jobtitle: 'Problem Solving through Programming in C',
         startYear: '2023',
         endYear: '2023'
     },
     {
-        id: 4,
+        id: 5,
         company: 'Internshala',
         jobtitle: 'Introduction to Machine Learning',
         startYear: '2023',
